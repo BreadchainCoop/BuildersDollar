6a824f5dc9f760c0049f63bebffcc9f0ef785979
hudsonhrh <hudsonhrh7@gmail.com>
2024-11-05 14:38:35 -0600

    updated test for array of addresses and return true

--- chain/test/validateProject.t.sol
+++ chain/test/validateProject.t.sol
@@ -5,20 +5,24 @@ import "forge-std/Test.sol";
 import "../src/mocks/MockEAS.sol";
 import "contracts/ValidateProject.sol";
 
-
-
 contract ProjectValidatorTest is Test {
     ProjectValidator private validator;
     MockEAS private mockEAS;
-    address private optimismFoundation = address(0x123);
+    address private optimismFoundation1 = address(0x123);
+    address private optimismFoundation2 = address(0x456);
     uint256 private roundStartTime = 1000;
     uint256 private roundEndTime = 2000;
 
     function setUp() public {
         mockEAS = new MockEAS();
+
+        address[] memory optimismFoundationAttestors = new address[](2);
+        optimismFoundationAttestors[0] = optimismFoundation1;
+        optimismFoundationAttestors[1] = optimismFoundation2;
+
         validator = new ProjectValidator(
             address(mockEAS),
-            optimismFoundation,
+            optimismFoundationAttestors,
             roundStartTime,
             roundEndTime
         );
@@ -33,8 +37,8 @@ contract ProjectValidatorTest is Test {
             time: 1500,
             expirationTime: 0,
             revocationTime: 0,
-            recipient: address(0x456),
-            attester: optimismFoundation,
+            recipient: address(0x789),
+            attester: optimismFoundation1,
             revocable: true,
             data: abi.encode(
                 "Grantee",
@@ -46,7 +50,8 @@ contract ProjectValidatorTest is Test {
         });
         mockEAS.setAttestation(uid, attestation);
 
-        validator.validateProject(uid);
+        bool result = validator.validateProject(uid);
+        assertTrue(result, "Validation should return true");
 
         bool isEligible = validator.eligibleProjects(uid);
         assertTrue(isEligible, "Project should be eligible");
@@ -61,8 +66,8 @@ contract ProjectValidatorTest is Test {
             time: 1500,
             expirationTime: 0,
             revocationTime: 0,
-            recipient: address(0x456),
-            attester: optimismFoundation,
+            recipient: address(0x789),
+            attester: optimismFoundation1,
             revocable: true,
             data: abi.encode(
                 "Grantee",
@@ -74,14 +79,21 @@ contract ProjectValidatorTest is Test {
         });
         mockEAS.setAttestation(uid, attestation);
 
-        validator.validateProject(uid);
+        bool firstResult = validator.validateProject(uid);
+        assertTrue(firstResult, "First validation should return true");
 
-        vm.expectRevert("Project already included");
-        validator.validateProject(uid);
+        bool secondResult = validator.validateProject(uid);
+        assertTrue(secondResult, "Second validation should return true even if already included");
+
+        bool isEligible = validator.eligibleProjects(uid);
+        assertTrue(isEligible, "Project should still be eligible");
     }
 
     function testValidateProjectInvalidAttester() public {
         bytes32 uid = keccak256(abi.encodePacked("test-attestation"));
+        // Attester not in the list
+        address invalidAttester = address(0x999);
+
         IEAS.Attestation memory attestation = IEAS.Attestation({
             uid: uid,
             schema: bytes32(0),
@@ -89,8 +101,8 @@ contract ProjectValidatorTest is Test {
             time: 1500,
             expirationTime: 0,
             revocationTime: 0,
-            recipient: address(0x456),
-            attester: address(0x789), // Invalid attester
+            recipient: address(0x789),
+            attester: invalidAttester,
             revocable: true,
             data: abi.encode(
                 "Grantee",
@@ -107,16 +119,16 @@ contract ProjectValidatorTest is Test {
     }
 
     function testValidateProjectNotInCurrentRound() public {
-        bytes32 uid = keccak256(abi.encodePacked("test-attestation"));
+        bytes32 uidEarly = keccak256(abi.encodePacked("test-attestation-early"));
         IEAS.Attestation memory attestationEarly = IEAS.Attestation({
-            uid: uid,
+            uid: uidEarly,
             schema: bytes32(0),
             refUID: bytes32(0),
             time: 500, // Before round start time
             expirationTime: 0,
             revocationTime: 0,
-            recipient: address(0x456),
-            attester: optimismFoundation,
+            recipient: address(0x789),
+            attester: optimismFoundation1,
             revocable: true,
             data: abi.encode(
                 "Grantee",
@@ -126,17 +138,34 @@ contract ProjectValidatorTest is Test {
                 "Application Approved"
             )
         });
-        mockEAS.setAttestation(uid, attestationEarly);
+        mockEAS.setAttestation(uidEarly, attestationEarly);
 
         vm.expectRevert("Attestation not in current round");
-        validator.validateProject(uid);
+        validator.validateProject(uidEarly);
 
-        IEAS.Attestation memory attestationLate = attestationEarly;
-        attestationLate.time = 2500; // After round end time
-        mockEAS.setAttestation(uid, attestationLate);
+        bytes32 uidLate = keccak256(abi.encodePacked("test-attestation-late"));
+        IEAS.Attestation memory attestationLate = IEAS.Attestation({
+            uid: uidLate,
+            schema: bytes32(0),
+            refUID: bytes32(0),
+            time: 2500, // After round end time
+            expirationTime: 0,
+            revocationTime: 0,
+            recipient: address(0x789),
+            attester: optimismFoundation1,
+            revocable: true,
+            data: abi.encode(
+                "Grantee",
+                "param2",
+                "param3",
+                "param4",
+                "Application Approved"
+            )
+        });
+        mockEAS.setAttestation(uidLate, attestationLate);
 
         vm.expectRevert("Attestation not in current round");
-        validator.validateProject(uid);
+        validator.validateProject(uidLate);
     }
 
     function testValidateProjectInvalidParam1() public {
@@ -148,8 +177,8 @@ contract ProjectValidatorTest is Test {
             time: 1500,
             expirationTime: 0,
             revocationTime: 0,
-            recipient: address(0x456),
-            attester: optimismFoundation,
+            recipient: address(0x789),
+            attester: optimismFoundation1,
             revocable: true,
             data: abi.encode(
                 "Not Grantee",
@@ -174,8 +203,8 @@ contract ProjectValidatorTest is Test {
             time: 1500,
             expirationTime: 0,
             revocationTime: 0,
-            recipient: address(0x456),
-            attester: optimismFoundation,
+            recipient: address(0x789),
+            attester: optimismFoundation1,
             revocable: true,
             data: abi.encode(
                 "Grantee",
