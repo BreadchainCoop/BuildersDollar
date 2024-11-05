2a7518ca5b86550db42608458de7049d011a4880
hudsonhrh <hudsonhrh7@gmail.com>
2024-11-05 13:33:37 -0600

    added basic interface and mock for EAS

---
+++ chain/src/interfaces/IEAS.sol
@@ -0,0 +1,20 @@
+// contracts/interfaces/IEAS.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IEAS {
+    struct Attestation {
+        bytes32 uid;
+        bytes32 schema;
+        bytes32 refUID;
+        uint64 time;
+        uint64 expirationTime;
+        uint64 revocationTime;
+        address recipient;
+        address attester;
+        bool revocable;
+        bytes data;
+    }
+
+    function getAttestation(bytes32 uid) external view returns (Attestation memory);
+}
